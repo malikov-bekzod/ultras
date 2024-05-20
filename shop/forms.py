6299040b569e4dfccac41b69c45fa499a9254eac
bekzod <malikovbekzodmansinov@gmail.com>
2024-05-20 07:48:13 +0500

    added new features

--- shop/forms.py
+++ shop/forms.py
@@ -1,14 +1,20 @@
 from django import forms
-from django.forms import inlineformset_factory
-from .models import Product, ProductImage
+from .models import Product
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = "__all__"
-
-
-ProductImageFormSet = inlineformset_factory(
-    Product, ProductImage, fields=("image",), extra=4, max_num=4
-)
+        fields = [
+            "name",
+            "description",
+            "color",
+            "size",
+            "price",
+            "category",
+            "brand",
+            "count",
+            "types",
+            "image",
+            "gender",
+        ]
--- shop/views.py
+++ shop/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from .models import Product, Category,Brands,Color,ClothingType
+from .models import Product, Category, Brands, Color, ClothingType
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
+from .forms import ProductForm
 
 
 # Create your views here.
 class ShopView(View):
     def get(self, request):
         products = Product.objects.all()
-        search = request.GET.get("search") or ''
-        price = request.GET.get("price") or ''
-        color = request.GET.get("color") or ''
-        typ = request.GET.get("typ") or ''
-        brand = request.GET.get("brand") or ''
+        search = request.GET.get("search") or ""
+        price = request.GET.get("price") or ""
+        color = request.GET.get("color") or ""
+        typ = request.GET.get("typ") or ""
+        brand = request.GET.get("brand") or ""
 
-        products = products.filter(name__icontains = search)
+        products = products.filter(name__icontains=search)
 
-        if price!="":
+        if price != "":
             if price == "20":
                 products = products.filter(Q(price__gte=0) & Q(price__lte=20))
             elif price == "50":
@@ -44,11 +46,52 @@ class ShopView(View):
             "colors": Color.choices,
             "types": ClothingType.choices,
         }
-        return render(request, "shop/shop.html",context)
+        return render(request, "shop/shop.html", context)
 
 
-class ShopDetailView(View):
-    def get(self,request,id):
-        pass
+class ShopDetailView(LoginRequiredMixin,View):
+    def get(self, request, id):
+        product = Product.objects.get(id=id)
+        context = {"product": product}
+        return render(request, "shop/shop-detail.html", context)
 
 
+class ProductCreateView(View):
+
+    def get(self, request):
+        form = ProductForm()
+        return render(request, "shop/product_form.html", {"form": form})
+
+    def post(self, request):
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("shop")
+        return render(request, "shop/product_form.html", {"form": form})
+
+
+class ProductUpdateView(View):
+
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        form = ProductForm(instance=product)
+        return render(request, "shop/product_form.html", {"form": form})
+
+    def post(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect("shop")
+        return render(request, "shop/product_form.html", {"form": form})
+
+
+class ProductDeleteView(View):
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        return render(request, "shop/product_confirm_delete.html", {"product": product})
+
+    def post(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        product.delete()
+        return redirect("shop")
